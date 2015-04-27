52a93e352d05ff9461ddea6fa1f50903e9ce64ef
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-04-26 22:40:00 -0500

    migrations

---
+++ migrations.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE restaurant_database;
+\c restaurant_database;
+
+CREATE TABLE foods(id SERIAL PRIMARY KEY, name VARCHAR(255), price INTEGER, allergens BOOLEAN);
+
+CREATE TABLE parties(id SERIAL PRIMARY KEY, table_number INTEGER, paid BOOLEAN);
+
+CREATE TABLE orders(id SERIAL PRIMARY KEY, party_name VARCHAR(255));
